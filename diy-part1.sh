08e1b5c956b0ce7dba09a516baf9b40badbc2b9d
Vangou <536415335@qq.com>
2021-07-15 19:47:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaulthttps://github.com/fw876/helloworld
 
 # Add a feed source
-echo 'src-git helloworld ' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
